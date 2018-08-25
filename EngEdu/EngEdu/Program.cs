ae05b7ebb8ed5d433604fc816ce22e5c2671f935
aun <nattapad10@gmail.com>
2018-08-25 16:10:09 +0700

    3nd year semester 1

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -10,10 +10,11 @@ namespace EngEdu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Year 2550 semester 1,I am an Engedu frsehman");
-            Console.WriteLine("Year 2550 semester 2,I learn to program in C sharp.");
+            Console.WriteLine("Year 2560 semester 1,I am an Engedu frsehman");
+            Console.WriteLine("Year 2560 semester 2,I learn to program in C sharp.");
             Console.WriteLine("Year 2561 semester 1,I start my programmer life");
             Console.WriteLine("Year 2561 semester 2,I am student of computer");
+            Console.WriteLine("Year 2562 semester 1,I am 3nd student of computer");
 
         }
     }
