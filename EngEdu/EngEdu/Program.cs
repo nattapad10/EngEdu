bee6262786ca94137720999fd7575c4c9f052f1b
aun <nattapad10@gmail.com>
2018-08-25 15:56:29 +0700

    4nd year semester 1

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -16,6 +16,7 @@ namespace EngEdu
             Console.WriteLine("Year 2561 semester 2,I am year 2 students");
             Console.WriteLine("Year 2562 semester 1,I am year 3 students");
             Console.WriteLine("Year 2562 semester 2,I am year 3 students");
+            Console.WriteLine("Year 2563 semester 1,I am year 4 students");
 
         }
     }
