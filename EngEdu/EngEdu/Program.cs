8571c38256f8402c484ccbebfbeee47451eff88a
aun <nattapad10@gmail.com>
2018-08-25 16:14:55 +0700

    4nd year semester 2

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -17,6 +17,7 @@ namespace EngEdu
             Console.WriteLine("Year 2562 semester 1,I am 3nd student of computer");
             Console.WriteLine("Year 2562 semester 2,I am 3nd student of computer will up to4");
             Console.WriteLine("Year 2563 semester 1,I am 4nd student of computer");
+            Console.WriteLine("Year 2563 semester 2,I am 4nd student of computer will up to5");
         }
     }
 }
